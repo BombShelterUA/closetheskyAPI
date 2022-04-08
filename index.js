09e86af470a82ffb685e72cfe5e7a35cea95b979
sanderpa <parfeniuksasha09@gmail.com>
2022-04-08 19:37:13 +0300

    Push commit (closed #39)

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 4001;
 
+app.get("/", (req, res) => {
+  res.send("<h1>This is closetheshy API</h1>");
+});
+
 app.listen(PORT, (e) => {
   e ? console.log(e) : console.log(`Server started at port ${PORT}`);
 });
