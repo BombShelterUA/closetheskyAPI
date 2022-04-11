da0036cf2b2906a579d9a3c0f946c61343914d3f
sanderpa <parfeniuksasha09@gmail.com>
2022-04-11 21:48:52 +0300

    Change require to import. Added connection to MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require("express");
-
-const app = express();
+import express from "express";
+import mongoose from "mongoose";
 
 const PORT = 4001;
+const DB_URL =
+  "mongodb+srv://admin:example123@testcloud.rltcz.mongodb.net/CloseTheSky?retryWrites=true&w=majority";
+
+const app = express();
+app.use(express.json());
+
+async function connectToDB() {
+  try {
+    await mongoose.connect(DB_URL);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connectToDB();
 
 app.get("/", (req, res) => {
   res.send("<h1>This is closetheshy API</h1>");
